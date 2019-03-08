26f84f7184897a395f8ba215d50ee02f98cbf9cd
Tsai Smith <Gon@localhost.localdomain>
2019-03-08 09:55:21 +0800

    Test

--- ML1-1.py
+++ ML1-1.py
@@ -10,7 +10,6 @@ plt.plot(0.3*t+1,0.2+np.zeros(100))
 plt.plot(0.2*t+1+0.1,0.8*t)
 plt.plot(0.2*t+1-0.1,-0.8*t)
 
-plt.plot(np.sin(5*t),np.cos(5*t))
 
 plt.scatter([0+0.5,2-0.5],[0.3,-0.3], s=120, facecolors='none', edgecolors='r')
 plt.axis([-1, 3, -2, 2])
